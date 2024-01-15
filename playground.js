04d763153c6dff6caef2aea010968b73aa71b5e2
Bartosz Pudo <pudobartosz95@gmail.com>
2024-01-15 13:05:41 +0100

    Added html support

--- playground.js
+++ playground.js
@@ -1,20 +1,11 @@
 "use strict";
 // Playground by Bartosz Pudo
 
-function uploader(content){
-    const style = document.querySelector("style");    
-    
-    switch(langArray){
-        case 'js':
-            const script = document.createElement("script");
-            script.type = "text/javascript";
-            script.innerHTML = content;
-        break;
-    }
-}
+// Creates the Array for languages that will be used later.
+const langArray = ['html', 'css', 'javascript'];
 
 // Creates a style element and appends it to the body.
-function createStyle(content){
+function createStyle(content) {
     let style = document.createElement("style");
     style.type = "text/css";
     style.innerHTML = content;
@@ -23,32 +14,103 @@ function createStyle(content){
     // console.log(style);
 }
 
-function createLangTextarea(){
-    // Creates the Array for languages that will be used later.
-        let langArray = ['html', 'css', 'javascript'];
+function createPlaygroundView() {
+        let playgroundView = document.createElement("div");
+        playgroundView.classList.add("playground-view");
+        document.body.appendChild(playgroundView);
+        let script = document.createElement("script");
+        script.type = "text/javascript";
+        playgroundView.appendChild(script);
+}
+
+function createLangTextarea() {
     // Creates the textareas and appends them to the body.
-        for(let i = 0; i <= langArray.length-1; i++){
+    let langTextareaContainer = document.createElement("div");
+    langTextareaContainer.classList.add("lang-textarea-container");
+    const langTextarea = document.createElement("textarea");
+    for (let i = 0; i <= langArray.length - 1; i++) {
         let langTextarea = document.createElement("textarea");
-            // Setting classlist and id to langTextarea.
-                langTextarea.classList.add("lang-textarea");    
-                langTextarea.setAttribute("id", langArray[i]);
-            // Appends the textarea to the body.
-                document.body.appendChild(langTextarea);
-                console.log(langTextarea);
-                langTextarea.addEventListener("focusout", () => {
-                    content = langTextarea.value;
-                    uploader(content);
-                });
-        }
-        
+        // Setting classlist and id to langTextarea.
+        langTextarea.classList.add("lang-textarea");
+        langTextarea.setAttribute("id", langArray[i]);
+        // Appends the textarea to the body.
+        langTextareaContainer.appendChild(langTextarea);
+
+        console.log(langTextarea);
+        langTextarea.addEventListener("focusout", () => {
+            let content = langTextarea.value;
+            uploader(content);
+        });
+    }
+    document.body.appendChild(langTextareaContainer);
+}
+
+function uploader(content) {        
+    switch (document.querySelector(`#${langArray[0]}`).id) {
+        case 'html':
+            let html = '';
+            html = content;
+            document.body.querySelector('div.playground-view').innerHTML = html;
+        break;
+        case 'css':
+            console.log("Currently not supported!");
+            // let pg_style = document.createElement("style");
+            // pg_style.type = 'text/css';
+            // pg_style = content;
+            // document.body.querySelector('div.playground-view').appendChild(pg_style);
+        break; 
+        case 'javascript':
+            console.log("Currently not supported!");
+            // const script = content.value;
+            // const playgroundView = document.querySelector('playgroundView');
+
+            // const startCompiling = new Function(script);
+            // eval(script);
+            // playgroundView.innerHTML = `function startCompiling(){${script}}`;
+
+            // startCompiling();
+        break;
+        default:
+            // DODAĆ Linie na końcu dla błędu
+            console.log("Wrong Language! Can't upload content! Watch Line ");
+        break;
+    }
 }
 
 // If the document is loaded, run the functions.
 document.addEventListener("DOMContentLoaded", () => {
     createLangTextarea();
-
+    createPlaygroundView();
     // Prebaked style info uploads to the style element.
-    const styleString = (``);
+    const styleString = (`
+    .lang-textarea{
+        width: 28%;
+        height: 300px;
+    }
+    #html{
+        color: white;
+        background-color: orange;
+    }
+    #css{
+        color: white;
+        background-color: blue;
+    }
+    #javascript{
+        background-color: yellow;
+    }
+    .lang-textarea-container{
+        display: flex;
+        flex-direction: row;
+        justify-content: space-evenly;
+    }
+    .playground-view{
+        display: flexbox;
+        margin: auto;
+        width: 98%;
+        height: 500px;
+        border: solid black 1px;
+    }
+    `);
 
     createStyle(styleString);
 });
